e58e19b10f16062150269d9c612d35fa788c3d42
Elaina Martineau <elainamartineau@gmail.com>
2017-01-02 21:25:58 -0700

    Add filtering

--- init.lua
+++ init.lua
@@ -1,22 +1,29 @@
 local _empty = {}
 
-local titter = function(t, k1)
+local titter = function(t, k1, filter)
 	k1 = k1 or "<top>"
+
+	local found = {}
 
 	local currentNode
 	local start = true
-
 	return function()
 		if start then
 			local nextKey, nextValue = next(t)
 			currentNode = {prevNode = nil, key = nextKey, value = nextValue, table = t}
 			start = false
-			return false, k1, t
+			return {
+				finish = false,
+				key = k1,
+				value = t,
+				last = true,
+				empty = not next(t)
+			}
 		else
 			if currentNode == nil then
 				return nil
 			else
-				local retFinish, retKey, retValue = false, currentNode.key, currentNode.value
+				local retFinish, retKey, retValue, retFound = false, currentNode.key, currentNode.value, found[currentNode.value]
 
 				local empty
 				if type(retValue) == "table" then
@@ -26,24 +33,44 @@ local titter = function(t, k1)
 				if not retKey then
 					local oldTable = currentNode.table
 					currentNode = currentNode.prevNode
-					return true, not (currentNode or _empty).key, not next(oldTable)
+					return {
+						finish = true,
+						last = not (currentNode or _empty).key,
+						empty = not next(oldTable)
+					}
 				end
 
 				currentNode.key, currentNode.value = next(currentNode.table, currentNode.key)
 
 				local last = not currentNode.key
 
+				local filtered
 				if type(retValue) == "table" then
-					local nextKey, nextValue = next(retValue)
-					currentNode = {
-						prevNode = currentNode,
-						key = nextKey,
-						value = nextValue,
-						table = retValue
-					}
+					if filter then
+						filtered = filter(retValue)
+					end
+
+					if not filtered and not found[retValue] then
+						found[retValue] = true
+						local nextKey, nextValue = next(retValue)
+						currentNode = {
+							prevNode = currentNode,
+							key = nextKey,
+							value = nextValue,
+							table = retValue
+						}
+					end
 				end
 
-				return retFinish, retKey, retValue, last, empty
+				return {
+					finish = retFinish,
+					key = retKey,
+					value = retValue,
+					last = last,
+					empty = empty,
+					found = not not retFound,
+					filtered = filtered
+				}
 			end
 		end
 	end
